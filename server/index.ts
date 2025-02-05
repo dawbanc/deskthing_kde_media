045a7b1508cc9c879a101e4e371df896aff65b0f
dawbanc <dawsonxbancroft@gmail.com>
2025-02-05 15:52:17 -0800

    Added necessary npm modules as well as found a better alternative than Firefox

--- server/index.ts
+++ server/index.ts
@@ -1,53 +1,92 @@
-/**
- * Welcome to the template app!
- *
- * This app is meant for developers to be able to reference while making their own apps. Nearly ever DeskThing feature is outlined here in some capacity to demonstrate the practical application of building an app for DeskThing!
- * Any questions, please join the discord server channel and ask there.
- *
- */
 
-/**
- * There are two connectors. DeskThing-server and DeskThing-client
- * To optimize your app, only use DeskThing-server inside the server and DeskThing-client inside the client
- *
- * Every app must both import DeskThing-server and export DeskThing-server to allow the DeskThing Server to link with your app
- */
 import { DeskThing, SocketData } from "deskthing-server";
-// Doing this is required in order for the server to link with DeskThing
 export { DeskThing };
 
-// The following imports are from other files that setup their own functions
-import { setupSettings } from "./settings";
-import { userInput } from "./userInput";
 import { sendImage, sendSampleData } from "./sendingData";
 
-/**
- * 
- *  ----------- Setup ------------------
- * 
- *  Every app needs the following two:
- * DeskThing.on('start', start)
- *
- * DeskThing.on('stop', stop)
- *
- * Both of these should be at the end of your index.ts page. 'start' is triggered when the server is started and 'stop' is triggered when the server is stopped.
- *
- *
- * The following start() function is triggered once the server starts. This is where all initialization should be done.
- */
+import * as dbus from 'dbus-next';
+import * as dbusnative from 'dbus-native';
+
+
+
+async function getCurrentPlayingData() {
+  try {
+    let dbus = require('dbus-next');
+    let bus = dbus.sessionBus();
+    let serviceNameBus = dbusnative.sessionBus();
+    let Variant = dbus.Variant;
+
+    let serviceName;
+
+    async function detectPlayers() {
+      const mediaPlayers = [
+        'org.kde.plasma.browser_integration',                 // This is the one with actual information
+        //'org.mpris.MediaPlayer2.firefox.instance_',   // This one might have the title plus source //TODO: Make this a backup
+      ]
+    
+      const names: string[] = await new Promise((resolve, reject) => {
+        serviceNameBus.listNames((err: any, results: any) => {
+          if (err) {
+            reject(err);
+            DeskThing.sendLog("ERR" + err);
+          } else {
+            resolve(results);
+            DeskThing.sendLog("RESULTS" + results);
+          }
+        });
+      })
+
+      DeskThing.sendLog("names: " + names);
+      
+      for (const prefix of mediaPlayers) {
+        const playerName = names.find((name: string) => name.startsWith(prefix))
+        if (playerName) {
+          serviceName = playerName;
+          DeskThing.sendLog('Found media player:' + serviceName);
+        }
+      }
+    }
+    
+    await detectPlayers();
+
+    const object = await bus.getProxyObject(serviceName, '/org/mpris/MediaPlayer2');
+    const player = await object.getInterface('org.mpris.MediaPlayer2.Player');
+    const properties = await object.getInterface('org.freedesktop.DBus.Properties');
+
+    const song_props = await properties.GetAll('org.mpris.MediaPlayer2.Player');
+    const metadata = song_props.Metadata.value;
+    DeskThing.sendLog('METADATA: ' + JSON.stringify(metadata, (_, v) => typeof v === 'bigint' ? v.toString() : v));
+
+    // Extract relevant information from the metadata
+    const artist = metadata?.['xesam:artist']?.value[0] as string | undefined;
+    const title = metadata?.['xesam:title'].avalue as string | undefined;
+    const album = metadata?.['xesam:album'].value as string | undefined;
+
+    DeskThing.sendLog(`Now Playing (Firefox):`);
+    DeskThing.sendLog(`Artist: ` + artist);
+    DeskThing.sendLog(`Title: ` + title);
+    DeskThing.sendLog(`Album: ` + album);
+    DeskThing.sendLog('---'); 
+
+
+  } catch (error) {
+    DeskThing.sendError('Error:' + error);
+  }
+}
+
 const start = async () => {
-  // This is being used to grab any associated data from the server once the app starts. This makes sure we dont try to initialize stuff twice if it already exists
   const Data = await DeskThing.getData();
 
-  setupSettings(Data);
-  userInput(Data);
-    // This will make Data.settings.theme.value equal whatever the user selects
+  getCurrentPlayingData();
 };
 
 const stop = async () => {
-  // Function called when the server is stopped
   DeskThing.sendLog('Server Stopped');
 };
+
+
+
+
 
 // Main Entrypoint of the server
 DeskThing.on("start", start);
@@ -55,18 +94,3 @@ DeskThing.on("start", start);
 // Main exit point of the server
 DeskThing.on("stop", stop);
 
-const handleRequest = async (socketData: SocketData) => {
-  switch (socketData.request) {
-    case 'sampleData':
-      sendSampleData()
-      break
-    case 'image':
-      sendImage()
-      break
-    default:
-      DeskThing.sendError('Invalid Request')
-      break
-  }
-}
-
-DeskThing.on('get', handleRequest)
