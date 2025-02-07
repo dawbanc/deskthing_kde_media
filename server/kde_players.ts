759f598da586d1a11246e40e389161c5a89cf391
dawbanc <dawsonxbancroft@gmail.com>
2025-02-07 07:06:11 -0800

    I think I have fixed the dbus polling issue

--- server/kde_players.ts
+++ server/kde_players.ts
@@ -1,54 +1,72 @@
 import { DeskThing } from 'deskthing-server';
 
-import * as dbus from 'dbus-next';
+import * as dbusnext from 'dbus-next';
 import * as dbusnative from 'dbus-native';
 
+let dbus;
+let bus;
+let serviceNameBus;
+let Variant;
+
+let serviceName;
+
+let properties;
+
+let initialized = false;
+
+async function init() {
+  let dbus = require('dbus-next');
+  let bus = dbus.sessionBus();
+  let serviceNameBus = dbusnative.sessionBus();
+  let Variant = dbus.Variant;
+
+  try {
+    async function detectPlayers() {
+      const mediaPlayers = [                              // PUT HIGHER PRIORITY SELECTIONS AT THE BOTTOM OF THE LIST
+          "org.mpris.MediaPlayer2.spotify",
+          "org.mpris.MediaPlayer2.firefox.instance_",
+          "org.mpris.MediaPlayer2.chromium.instance",
+          "org.mpris.MediaPlayer2.google-chrome.instance_",
+          "org.mpris.MediaPlayer2.YoutubeMusic",
+          "org.kde.kdeconnect.mpris_",
+          "org.kde.plasma.browser_integration", 
+      ]
+  
+      const names: string[] = await new Promise((resolve, reject) => {
+      serviceNameBus.listNames((err: any, results: any) => {
+          if (err) {
+          reject(err);
+          } else {
+          resolve(results);
+          }
+      });
+      })
+  
+      for (const prefix of mediaPlayers) {
+      const playerName = names.find((name: string) => name.startsWith(prefix))
+      if (playerName) {
+          serviceName = playerName;
+          //DeskThing.sendLog('Found media player:' + serviceName);
+      }
+      }
+      }
+  
+      await detectPlayers();
+  
+      const object = await bus.getProxyObject(serviceName, '/org/mpris/MediaPlayer2');
+      const player = await object.getInterface('org.mpris.MediaPlayer2.Player');
+      properties = await object.getInterface('org.freedesktop.DBus.Properties');
+  } catch (error) {
+    DeskThing.sendError(error);
+  }
+  initialized = true;
+}
 
 export async function sendCurrentPlayingData() {
+  if (!initialized) {
+    init();
+  }
   try {
-    let dbus = require('dbus-next');
-    let bus = dbus.sessionBus();
-    let serviceNameBus = dbusnative.sessionBus();
-    let Variant = dbus.Variant;
-
-    let serviceName;
-
-    async function detectPlayers() {
-    const mediaPlayers = [                              // PUT HIGHER PRIORITY SELECTIONS AT THE BOTTOM OF THE LIST
-        "org.mpris.MediaPlayer2.spotify",
-        "org.mpris.MediaPlayer2.firefox.instance_",
-        "org.mpris.MediaPlayer2.chromium.instance",
-        "org.mpris.MediaPlayer2.google-chrome.instance_",
-        "org.mpris.MediaPlayer2.YoutubeMusic",
-        "org.kde.kdeconnect.mpris_",
-        "org.kde.plasma.browser_integration", 
-    ]
-
-    const names: string[] = await new Promise((resolve, reject) => {
-    serviceNameBus.listNames((err: any, results: any) => {
-        if (err) {
-        reject(err);
-        } else {
-        resolve(results);
-        }
-    });
-    })
-
-    for (const prefix of mediaPlayers) {
-    const playerName = names.find((name: string) => name.startsWith(prefix))
-    if (playerName) {
-        serviceName = playerName;
-        //DeskThing.sendLog('Found media player:' + serviceName);
-    }
-    }
-    }
-
-    await detectPlayers();
-
-    const object = await bus.getProxyObject(serviceName, '/org/mpris/MediaPlayer2');
-    const player = await object.getInterface('org.mpris.MediaPlayer2.Player');
-    const properties = await object.getInterface('org.freedesktop.DBus.Properties');
-
     const song_props = await properties.GetAll('org.mpris.MediaPlayer2.Player');
     const metadata = song_props.Metadata.value;
     //DeskThing.sendLog('METADATA: ' + JSON.stringify(metadata, (_, v) => typeof v === 'bigint' ? v.toString() : v));
